eb7773afac996fdab73d4ca0cd099f4229f1927b
oshhh <osheen18059@iiitd.ac.in>
2020-08-12 15:14:25 +0530

    seperated algo and fetching classroom coursework

---
+++ classroom_suggestions.js
@@ -0,0 +1,167 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+var getSuggestions = require('./get_suggestions');
+
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/classroom.courses', 'https://www.googleapis.com/auth/classroom.coursework.students'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function listCourseWork(auth, courseId, callback) {
+  const classroom = google.classroom({version: 'v1', auth});
+  classroom.courses.courseWork.list({
+    courseId: courseId,
+    orderBy: "dueDate desc",
+    courseWorkStates: ["DRAFT", "PUBLISHED"],
+    pageSize: 1000,
+  }, (err, res) => {
+    if (err) return console.error('The API returned an error: ' + err);
+    const courseWork = res.data.courseWork;
+    callback(courseWork, auth);
+  });
+}
+
+function listCourses(auth, callback) {
+  const classroom = google.classroom({version: 'v1', auth});
+  classroom.courses.list({
+    pageSize: 1000,
+  }, (err, res) => {
+    if (err) return console.error('The API returned an error: ' + err);
+    const courses = res.data.courses;
+    callback(courses, auth);
+  });
+}
+
+function printCourses(courses) {
+  if (courses && courses.length) {
+    console.log('courses:');
+    courses.forEach((course) => {
+      console.log(`${course.id}, ${course.name}`);
+    });
+  } else {
+    console.log('No courses found.');
+  }
+}
+
+function printCourseWorks(courseWorks) {
+  if (courseWorks && courseWorks.length) {
+    console.log('Coursework:');
+    courseWorks.forEach((courseWork) => {
+      console.log(`${courseWork.courseId}: ${courseWork.title} \n${JSON.stringify(courseWork)}`);
+    });
+  } else {
+    console.log('No coursework found.');
+  }
+}
+
+function getDueDateSuggestions(students, duration, minDueDate, maxDueDate, callback) {
+
+  fs.readFile('credentials.json', (err, content) => {
+	  if (err) return console.log('Error loading client secret file:', err);
+
+    var commonStudents = {};
+    var courses = [];
+    for(var i = 0; i < students.length; ++ i) {
+        for(var j = 0; j < students[i]["courses"].length; ++ j) {
+            if(commonStudents[students[i]["courses"][j]] == null) {
+                courses.push(students[i]["courses"][j]);
+                commonStudents[students[i]["courses"][j]] = 0;
+            }
+            commonStudents[students[i]["courses"][j]] ++;
+        }
+    }
+
+	  authorize(JSON.parse(content), (auth) => {
+	  	allCourseWork = []
+	  	var count = 0;
+	    for(var i = 0; i < courses.length; ++ i) {
+	      listCourseWork(auth, courses[i], (courseWork, auth) => {
+	  			if(courseWork != null) {
+	  				courseWork.forEach((c) => { 
+              if(c.scheduledTime == null) {
+                c.startDate = Date.parse(c.creationTime);
+              } else {
+                c.startDate = Date.parse(c.scheduledTime);
+              }
+              c.endDate = Date.parse(`${c.dueDate.year}-${c.dueDate.month}-${c.dueDate.day} ${c.dueTime.hours}:${c.dueTime.minutes}:00Z`);
+              allCourseWork.push(c); 
+            })
+	  			};
+	        count ++;
+	        if(count == courses.length) {
+	          callback(getSuggestions.suggestDueDate(duration, minDueDate, maxDueDate, allCourseWork, students));
+	        }
+	      });
+	    }
+	  });
+	});
+}
+
+
+
+function main() {
+	students = [{"roll_no": "S1", "courses": ["122593778141", "122593778147", "122593792063"]}, {"roll_no": "S2", "courses" : ["122593778141", "122593778147", "122593792068"]}];
+	duration = {"date": 1, "hours": 0, "minutes": 0};
+    minDueDate = new Date();
+    maxDueDate = new Date();
+	maxDueDate.setDate(maxDueDate.getDate() + 3);
+	getDueDateSuggestions(students, duration, minDueDate, maxDueDate, (suggestions) => console.log(suggestions));
+}
+
+
+main();
--- get_suggestions.js
+++ get_suggestions.js
@@ -1,115 +1,24 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+/*
+	~~ Parameters ~~
+	duration: datatype - JSON, format - {date: .., hours: .., minutes: ..}
+	minDueDate: datatype - Date
+	maxDueDate: datatype - Date
+	allCourseWork: datatype - list of courseWorks, format - courseWork.startDate (datatype - Date), courseWork.endDate (datatype - Date)
+	students: datatype - list of JSON, format - list of JSON {"roll_no": .., "courses": ..}
+*/
+function suggestDueDate(duration, minDueDate, maxDueDate, allCourseWork, students) {
+	var commonStudents = {};
+	for(var i = 0; i < students.length; ++ i) {
+		for(var j = 0; j < students[i]["courses"].length; ++ j) {
+			if(commonStudents[students[i]["courses"][j]] == null) {
+				commonStudents[students[i]["courses"][j]] = 0;
+			}
+			commonStudents[students[i]["courses"][j]] ++;
+		}
+	}
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/classroom.courses', 'https://www.googleapis.com/auth/classroom.coursework.students'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-function listCourseWork(auth, courseId, callback) {
-  const classroom = google.classroom({version: 'v1', auth});
-  classroom.courses.courseWork.list({
-    courseId: courseId,
-    orderBy: "dueDate desc",
-    courseWorkStates: ["DRAFT", "PUBLISHED"],
-    pageSize: 1000,
-  }, (err, res) => {
-    if (err) return console.error('The API returned an error: ' + err);
-    const courseWork = res.data.courseWork;
-    callback(courseWork, auth);
-  });
-}
-
-function listCourses(auth, callback) {
-  const classroom = google.classroom({version: 'v1', auth});
-  classroom.courses.list({
-    pageSize: 1000,
-  }, (err, res) => {
-    if (err) return console.error('The API returned an error: ' + err);
-    const courses = res.data.courses;
-    callback(courses, auth);
-  });
-}
-
-function printCourses(courses) {
-  if (courses && courses.length) {
-    console.log('courses:');
-    courses.forEach((course) => {
-      console.log(`${course.id}, ${course.name}`);
-    });
-  } else {
-    console.log('No courses found.');
-  }
-}
-
-function printCourseWorks(courseWorks) {
-  if (courseWorks && courseWorks.length) {
-    console.log('Coursework:');
-    courseWorks.forEach((courseWork) => {
-      console.log(`${courseWork.courseId}: ${courseWork.title} \n${JSON.stringify(courseWork)}`);
-    });
-  } else {
-    console.log('No coursework found.');
-  }
-}
-
-function suggestDueDate(duration, maxDueDate, allCourseWork, commonStudents, callback) {
 	// for each date call calculateScore and order suggestions score 
-	suggestion = new Date();
+	suggestion = new Date(minDueDate);
 	lastDate = new Date(maxDueDate);
 	lastDate.setDate(lastDate.getDate() - duration.date);
 	lastDate.setHours(lastDate.getHours() - duration.hours);
@@ -131,22 +40,14 @@ function suggestDueDate(duration, maxDueDate, allCourseWork, commonStudents, cal
 		suggestion.setDate(suggestion.getDate() + 1);
 	}
 
-	callback(suggestions);
+	return suggestions;
 }
 
 function calculateScore(startDate, endDate, allCourseWork, commonStudents) {
 	var score = 0;
 	for(var i = 0; i < allCourseWork.length; ++ i) {
 		var courseWork = allCourseWork[i];
-		var c_startDate, c_endDate;
-		if(courseWork.scheduledTime == null) {
-			c_startDate = Date.parse(courseWork.creationTime);
-		} else {
-			c_startDate = Date.parse(courseWork.scheduledTime);
-		}
-		c_endDate = Date.parse(`${courseWork.dueDate.year}-${courseWork.dueDate.month}-${courseWork.dueDate.day} ${courseWork.dueTime.hours}:${courseWork.dueTime.minutes}:00Z`);
-
-		score += commonStudents[courseWork.courseId] * fractionalOverlap(startDate, endDate, c_startDate, c_endDate);
+		score += commonStudents[courseWork.courseId] * fractionalOverlap(startDate, endDate, courseWork.startDate, courseWork.endDate);
 	}
 	return score;
 }
@@ -164,53 +65,7 @@ function fractionalOverlap(c1_startDate, c1_endDate, c2_startDate, c2_endDate) {
 	}
 }
 
-function getDueDateSuggestions(students, duration, maxDueDate, callback) {
 
-  fs.readFile('credentials.json', (err, content) => {
-	  if (err) return console.log('Error loading client secret file:', err);
-
-
-	  var commonStudents = {};
-	  var courses = [];
-	  for(var i = 0; i < students.length; ++ i) {
-	  	for(var j = 0; j < students[i]["courses"].length; ++ j) {
-	  		if(commonStudents[students[i]["courses"][j]] == null) {
-	  			courses.push(students[i]["courses"][j]);
-	  			commonStudents[students[i]["courses"][j]] = 0;
-	  		}
-	  		commonStudents[students[i]["courses"][j]] ++;
-	  	}
-	  }
-
-	  authorize(JSON.parse(content), (auth) => {
-	  	allCourseWork = []
-	  	var count = 0;
-	    for(var i = 0; i < courses.length; ++ i) {
-	      listCourseWork(auth, courses[i], (courseWork, auth) => {
-	  			if(courseWork != null) {
-	  				courseWork.forEach((c) => { allCourseWork.push(c); })
-	  			};
-	        count ++;
-	        if(count == courses.length) {
-	          suggestDueDate(duration, maxDueDate, allCourseWork, commonStudents, (dueDateSuggestions) => {
-	            console.log(dueDateSuggestions);
-	          });
-	        }
-	      });
-	    }
-	  });
-	});
-}
-
-
-
-function main() {
-	students = [{"id": "S1", "courses": ["122593778141", "122593778147", "122593792063"]}, {"id": "S2", "courses" : ["122593778141", "122593778147", "122593792068"]}];
-	duration = {"date": 1, "hours": 0, "minutes": 0};
-	maxDueDate = new Date();
-	maxDueDate.setDate(maxDueDate.getDate() + 3);
-	getDueDateSuggestions(students, duration, maxDueDate, (suggestions) => console.log(suggestions));
-}
-
-
-main();
+module.exports = {
+  suggestDueDate: suggestDueDate
+};
