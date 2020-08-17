c5f83e3145d92086204728c59451fe24a3136169
oshhh <osheen18059@iiitd.ac.in>
2020-08-17 21:55:11 +0530

    .

--- server/fetch_events.js
+++ server/fetch_events.js
@@ -90,7 +90,6 @@ function listEvents(auth, calendarId, start, callback) {
 function getAllEvents(start, callback) {
 // Load client secrets from a local file.
   credentials_json = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-  if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
   authorize(credentials_json, (auth) => {
     listCalendars(auth, (auth, calendars) => {
